b012cd739bf55acea8165e4c1a25880b32bbdc4c
Grant Moore <chinchalinchin@gmail.com>
2022-05-05 08:53:29 -0400

    add boilerplate response to send mail endpoint

--- lambdas/email/send/lambda_function.py
+++ lambdas/email/send/lambda_function.py
@@ -1,7 +1,29 @@
 import os
+import json
+
 import boto3
 
 
-
 def lambda_handler(event, context):
-    print(event)
+    raw = event.get('body', None)
+    
+    if raw is not None:
+        body = json.loads(raw)
+        print(body)
+
+    response = {
+        "mesasge": "Email Sent"
+    }
+    
+    return {
+          "isBase64Encoded": False,
+          "statusCode": 200,
+          "headers": {
+              "Access-Control-Allow-Headers": "Content-Type",
+              "Allow": "OPTIONS, POST",
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Methods": "OPTIONS,POST",
+              "Content-Type": "application/json"
+          },
+          "body": json.dumps(response)
+    } 
