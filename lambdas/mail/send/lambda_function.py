1e899bcc9f73683465d9b35c5ea06b63b3830c81
Grant Moore <chinchalinchin@gmail.com>
2022-05-05 18:45:37 -0400

    update send email lambda to send email

--- lambdas/mail/send/lambda_function.py
+++ lambdas/mail/send/lambda_function.py
@@ -2,28 +2,81 @@ import os
 import json
 
 import boto3
+from botocore.exceptions import ClientError
 
+
+sender = os.environ['MailSender']
+recipient = os.environ['MailRecipient']
+
+def ses():
+    return boto3.client('ses')
+
+def format_msg(body):
+    msg = body['message']
+    reason = body['reason'] if body['subreason'] == 'None' else body['reason'] + ' ' + body['subreason']
+    name = body['first'] + ' ' + body['last']
+    email = body['email']
+    return f"""
+    Dear Cumberland Cloud,
+
+        The reason for this message is: {reason}.
+
+        {msg}
+
+        Please contact me at {email} at your earliest convenience.
+
+    Regards,
+    
+    {name}
+    """
 
 def lambda_handler(event, context):
     raw = event.get('body', None)
     
     if raw is not None:
         body = json.loads(raw)
-        print(body)
+        try:
+            ses_response = ses().send_mail(
+                Source=sender,
+                Destination={
+                    'ToAddresses': [
+                        recipient,
+                    ],
+                    'CcAddresses': [
+                        body['email'],
+                    ],
+                },
+                Message={
+                    'Subject': {
+                        'Data': 'Message for the Cumberland Cloud',
+                    },
+                    'Body': {
+                        'Text': {
+                            'Data': format_msg(body),
+                        }
+                    }
+                }
+            )
+            response, status = ses_response, 200
 
-    response = {
-        "mesasge": "Email Sent"
-    }
-    
+        except ClientError as e:
+            response, status = e, 500
+
+    else:
+        response = {
+            'message': 'No body in POST'
+        }
+        status = 400
+
     return {
-          "isBase64Encoded": False,
-          "statusCode": 200,
-          "headers": {
-              "Access-Control-Allow-Headers": "Content-Type",
-              "Allow": "OPTIONS, POST",
-              "Access-Control-Allow-Origin": "*",
-              "Access-Control-Allow-Methods": "OPTIONS,POST",
-              "Content-Type": "application/json"
-          },
-          "body": json.dumps(response)
-    } 
+            "isBase64Encoded": False,
+            "statusCode": status,
+            "headers": {
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Allow": "OPTIONS, POST",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST",
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps(response)
+    }
