2826cca64cbd9a2c27d1156d96e041d3f94889b0
Grant Moore <chinchalinchin@gmail.com>
2022-05-16 11:09:27 -0400

    add lambda function for article

---
+++ lambdas/blog/article/lambda_function.py
@@ -0,0 +1,43 @@
+import json
+import os
+
+import boto3 
+from botocore.exceptions import ClientError
+
+USERPOOL = os.environ['USERPOOL']
+CLIENT = os.environ['CLIENT']
+
+def cognito_idp():
+    return boto3.client('cognito-idp')
+
+def lambda_handler(event, context):
+    body = event.get('body', None)
+
+    if body is not None:
+        response = json.loads(body)
+        try:
+            response = {
+                'something'
+            }
+            status = 200
+        except ClientError as e:
+            response, status = e, 500
+        
+    else:
+        response = {
+          'message' : 'No body in request'
+        }
+        status = 400
+
+    return {
+          "isBase64Encoded": False,
+          "statusCode": status,
+          "headers": {
+              "Access-Control-Allow-Headers": "Content-Type",
+              "Allow": "OPTIONS, POST",
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Methods": "OPTIONS,POST",
+              "Content-Type": "application/json"
+          },
+          "body": json.dumps(response)
+    } 
