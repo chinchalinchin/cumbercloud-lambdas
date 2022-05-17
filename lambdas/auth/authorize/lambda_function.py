efc0da9b13f4c893bdbad253dd21ef6729653428
Grant Moore <chinchalinchin@gmail.com>
2022-05-17 08:15:54 -0400

    add methods to authorize lambda to generate api gateway policy

--- lambdas/auth/authorize/lambda_function.py
+++ lambdas/auth/authorize/lambda_function.py
@@ -1,3 +1,4 @@
+from ctypes import Union
 import json
 import os
 import pprint
@@ -9,22 +10,25 @@ import boto3
 from jwt.algorithms import RSAAlgorithm
 from botocore.exceptions import ClientError
 
+ACCOUNT=os.environ['ACCOUNT']
+API=os.environ['API']
 USERPOOL = os.environ['USERPOOL']
 CLIENT = os.environ['CLIENT']
 APPLICATION = os.environ['APPLICATION']
 REGION = os.environ['REGION']
 
-def key_url():
+def key_url() -> str:
     return 'https://cognito-idp.{}.amazonaws.com/{}/.well-known/jwks.json'.format(REGION, USERPOOL)
 
-def lambda_handler(event, context):
+def lambda_handler(event: dict, context: dict):
     pprint.pprint(event)
+    pprint.pprint(context)
     token = event['authorizationToken'].split(' ')[-1]
     header = jwt.get_unverified_header(token)
 
     keys = requests.get(key_url()).json()
     kid = header['kid']
-    
+
     jwk = find_key(keys, kid)
 
     if jwk is not None:
@@ -37,11 +41,26 @@ def lambda_handler(event, context):
         except Exception as e:
             return
 
-def find_key(keys, kid):
+def policy(context: dict, deny: bool) -> dict:
+    return {
+        "principalId": context['authorizer']['principalId'],
+        "policyDocument": {
+            "Version": "2012-10-17",
+            "Statement": [
+            {
+                "Action": "execute-api:Invoke",
+                "Effect": "Deny" if deny else "Allow",
+                "Resource": f"arn:aws:execute-api:{REGION}:{ACCOUNT}:{API}/*"
+            }
+            ]
+        }
+    }
+
+def find_key(keys, kid) -> Union[dict,None]:
     for key in keys:
         if key['kid'] == kid:
             return key
     return None
 
-def decode_token(token, publicKey):
+def decode_token(token: str, publicKey: str)->dict:
     return jwt.decode(token, publicKey, algorithms=['RS256'], audience=CLIENT)
