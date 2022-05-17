16b3c610857cc40b30d237838174318ea722ff1f
Grant Moore <chinchalinchin@gmail.com>
2022-05-17 08:06:37 -0400

    decode cognito token

--- lambdas/auth/authorize/lambda_function.py
+++ lambdas/auth/authorize/lambda_function.py
@@ -2,43 +2,46 @@ import json
 import os
 import pprint
 
+import requests
+import jwt
 import boto3 
+
+from jwt.algorithms import RSAAlgorithm
 from botocore.exceptions import ClientError
 
 USERPOOL = os.environ['USERPOOL']
 CLIENT = os.environ['CLIENT']
 APPLICATION = os.environ['APPLICATION']
+REGION = os.environ['REGION']
+
+def key_url():
+    return 'https://cognito-idp.{}.amazonaws.com/{}/.well-known/jwks.json'.format(REGION, USERPOOL)
 
 def lambda_handler(event, context):
     pprint.pprint(event)
-    body = event.get('body', None)
+    token = event['authorizationToken'].split(' ')[-1]
+    header = jwt.get_unverified_header(token)
 
-    if body is not None:
-        parsed_body = json.loads(body)
-        pprint.pprint(parsed_body)
+    keys = requests.get(key_url()).json()
+    kid = header['kid']
+    
+    jwk = find_key(keys, kid)
+
+    if jwk is not None:
+        public_key = RSAAlgorithm.from_jwk(json.dumps(jwk))
+
         try:
-            response ={
-                'message': 'something'
-            }
-            status = 200
-        except ClientError as e:
-            response, status = e, 500
-        
-    else:
-        response = {
-          'message' : 'No body in request'
-        }
-        status = 400
+            decoded = decode_token(token, public_key)
+            pprint.pprint(decoded)
 
-    return {
-          "isBase64Encoded": False,
-          "statusCode": status,
-          "headers": {
-              "Access-Control-Allow-Headers": "Content-Type",
-              "Allow": "OPTIONS, POST",
-              "Access-Control-Allow-Origin": "*",
-              "Access-Control-Allow-Methods": "OPTIONS,POST",
-              "Content-Type": "application/json"
-          },
-          "body": json.dumps(response)
-    } 
+        except Exception as e:
+            return
+
+def find_key(keys, kid):
+    for key in keys:
+        if key['kid'] == kid:
+            return key
+    return None
+
+def decode_token(token, publicKey):
+    return jwt.decode(token, publicKey, algorithms=['RS256'], audience=CLIENT)
